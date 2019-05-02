f2129c0426c2d8f82e46ef0dd73f7a52db56022b
Mariusz Ozdoba <mariuszozdoba@e902ab1f.local>
2019-05-02 14:23:53 +0200

    config

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 class Config(object):
     # ...
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql+mysqlconnector://root:root@0.0.0.0:3307/flask_framework'
+        'mysql+mysqlconnector://root:root@mysql_flask:3306/flask_framework'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super_secret'
